4d1157ace7babaf1b61bdfce4b10b20ce7390327
Johannes Hupfeld <j.hupfeld@gmail.com>
2020-01-24 21:46:45 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,45 +2,27 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
-  def initialize(account_name)
-    @name = account_name
+  def initialize(name)
+    @name = name
     @balance = 1000
     @status = "open"
   end
 
-  def deposit(money)
-    @balance += money
+  def deposit(amount)
+    self.balance+=amount
   end
 
   def display_balance
-    "Your balance is $#{@balance}."
+    "Your balance is $#{self.balance}."
   end
 
   def valid?
-    @status == "open" && @balance > 0 ? true : false
+    return true if self.status == "open" && self.balance > 0 else return false
   end
 
   def close_account
-    @status = "closed"
-  end
-end
-  def balance=(balance)
-    @balance
-  end
-
-  def valid?
-    (!!status == 'open') && (!!balance > 0)
-  end
-
-def account_hash
-    instance_variables.map do |var|
-        [var[1..-1].to_sym, instance_variable_get(var)]
-    end.to_h
+    self.status = "closed"
+    self.balance = 0
   end
 
 end
-
-
-
- avi = BankAccount.new("Avi")
- avi.account_hash
